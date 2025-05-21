17b259cfd77b564812164ed4bcfa31274db8de0c
bynd1u <semenbindu@gmail.com>
2025-05-21 16:16:35 +0200

    todo editing

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Item = ({ item, setTodos }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const inputRef = useRef(null);
+
   const completeTodo = () => {
     setTodos((prev) =>
       prev.map((todo) =>
@@ -8,17 +11,64 @@ export const Item = ({ item, setTodos }) => {
       )
     );
   };
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  useEffect(() => {
+    if (isEdit && inputRef) {
+      inputRef.current.focus();
+      inputRef.current.setSelectionRange(
+        inputRef.current.value.length,
+        inputRef.current.value.length
+      );
+    }
+  }, [isEdit]);
+
+  const handleInputSumbit = () => {
+    setIsEdit(false);
+  };
+
+  const handleInputChange = (e) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === item.id ? { ...todo, title: e.target.value } : todo
+      )
+    );
+  };
+
+  const handleInputBlur = () => {
+    setIsEdit(false);
+  };
+
   return (
     <li id={item.id} className="todo-item">
-      <button className="item-left" onClick={completeTodo}>
-        <p style={item.isCompleted ? { textDecoration: "line-through" } : {}}>
-          {item.title}
-        </p>
-      </button>
-      <div className="item-right">
-        <button>Edit</button>
-        <button>Delete</button>
-      </div>
+      {isEdit ? (
+        <form className="input-form" onSubmit={handleInputSumbit}>
+          <input
+            ref={inputRef}
+            type="text"
+            defaultValue={item.title}
+            onChange={handleInputChange}
+            onBlur={handleInputBlur}
+          />
+        </form>
+      ) : (
+        <>
+          <button className="item-left" onClick={completeTodo}>
+            <p
+              style={item.isCompleted ? { textDecoration: "line-through" } : {}}
+            >
+              {item.title}
+            </p>
+          </button>
+          <div className="item-right">
+            <button onClick={handleEdit}>Edit</button>
+            <button>Delete</button>
+          </div>
+        </>
+      )}
     </li>
   );
 };
